af21e3807193c7109cbddbdad2b592d3da27c08d
steveplayshorn <steveplayshorn@gmail.com>
2021-06-14 17:11:01 -0500

    Created landing page and dashboard. Setup complete for passport js.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,75 @@ const router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const salt = 10
+const session = require('express-session')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+
+
+router.use(session({
+    secret: 'secretkey',
+    resave: true,
+    saveUninitialized: true
+}))
+
+router.use(passport.initialize())
+router.use(passport.session())
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+
+passport.use(new localStrategy({usernameField: 'email'}, (email, password, done) => {
+    User.findOne({email: email}, (err, user) => {
+        if(err) return done(err)
+        if(!user) {
+            return done(null, false, {message: 'Email not found'})
+        }
+
+        bcrypt.compare(password, user.password, (err, res) => {
+            if(err) return done(err)
+            if(res === false) return done(null, false,{ message: 'Wrong password'})
+            return done(null, user)
+        })
+    })
+}))
+
+router.get('/setup', async (req, res) => {
+	const exists = await User.exists({ email: "git@hub.com" });
+
+	if (exists) {
+		res.redirect('/user/login');
+		return;
+	};
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash("pass", salt, function (err, hash) {
+			if (err) return next(err);
+			
+			const newAdmin = new User({
+				email: "git@hub.com",
+				password: hash
+			});
+
+			newAdmin.save();
+
+			res.redirect('/user/login');
+		});
+	});
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/user/login')
+}
+
 
 router.get('/', (req, res) => {
     User.find({})
@@ -11,8 +80,21 @@ router.get('/', (req, res) => {
     .then(users => res.render('users', {users}))
 })
 
-router.get('/new', (req, res) => {
-    res.render('newUser')
+router.get('/dashboard', isLoggedIn, (req, res) => {
+    res.render('dashboard')
+})
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user/dashboard',
+    failureRedirect: '/user/login?error=true'
+}))
+
+router.get('/register', (req, res, next) => {
+    res.render('register')
 })
 
 router.get('/:id', (req, res) => {
@@ -23,15 +105,17 @@ router.get('/:id', (req, res) => {
     .then(property => res.render('user', {property}))
 })
 
-router.post('/', (req, res, next) => {
+
+
+router.post('/register', (req, res, next) => {
     console.log(req.body.email)
-    bcrypt.genSalt(salt, function(err, salt) {
-        bcrypt.hash(req.body.password, salt, function(err, hash) {
+    bcrypt.genSalt(salt, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
             User.create({
                 email: req.body.email,
                 password: hash
             })
-            .then(user => res.render('user'))
+            .then(user => res.render('login'))
             .catch(next)
         });
     });
--- index.js
+++ index.js
@@ -2,23 +2,44 @@ const express = require('express')
 const app = express()
 const ejsLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
 
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
 
+app.get('/', (req, res) => {
+    res.render('index.html')})
+
 const propertyController = require('./controllers/propertyController')
 app.use('/property', propertyController)
 
 const userController = require('./controllers/userController')
+const User = require('./models/user')
 app.use('/user', userController)
 
 app.set('port', process.env.PORT || 3000)
+
+
 
 app.listen(app.get('port'), () => {
     console.log(`✅ PORT: ${app.get('port')} 🌟`)
